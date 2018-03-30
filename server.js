384f5d02187e4a7b41c1276c6ef22292bf53ee4e
Brian R Murphy <BrianRMurphy89@gmail.com>
2018-03-30 16:59:09 -0400

    server.js completed and beer_controller.js framework complete

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
-app.get('/', (req,res) => {
-    res.send('Sup World')
+app.use(express.urlencoded({extended:false}));
+app.use(express.static('public'));
+app.use(express.json());
+
+const beerController = require('./controllers/beer_controller.js')
+app.use('/beers' , beerController);
+
+
+mongoose.connect('mongodb://localhost:27017/beers');
+mongoose.connection.once('Open', () => {
+    console.log('Mongod is listening');
 })
-
 
 app.listen(port, () => {
     console.log('Terminal is listening');
